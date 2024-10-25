1e89cc9d1b8d8610ef798c0bf3ba195e18d0d27d
Nelson Carvalho <nelsoncarvalho@10.0.0.104>
2024-10-25 14:43:08 -0400

    gh

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,12 +12,12 @@ const config: Config = {
   url: "https://github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Nelson-DevStack.github.io",
+  baseUrl: "/docusaurus-template",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Nelson-DevStack", // Usually your GitHub org/user name.
-  projectName: "Nelson-DevStack.github.io", // Usually your repo name.
+  projectName: "docusaurus-template", // Usually your repo name.
   deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
